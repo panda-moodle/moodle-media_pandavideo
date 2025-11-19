967a3e70b4dc56d54add64c8962e1af405dddae9
Eduardo Kraus <kraus@eduardokraus.com>
2025-11-19 07:18:51 -0300

    Opening brace must not be followed by a blank line

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -36,7 +36,6 @@ use core_privacy\local\request\context;
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class provider implements null_provider {
-
     /**
      * Get reason.
      *
